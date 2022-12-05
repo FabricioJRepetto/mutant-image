d21763633d39a5a63998f91155d690f8d683b62c
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2022-12-05 18:40:10 -0300

    read me

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [image, setImage] = useState()
+    const [preview, setPreview] = useState()
+
+    const img = (e) => {
+        e.preventDefault()
+        const file = Array.from(e.target.files)
+        console.log(file)
+        setImage(() => file)
+    }
+
+    useEffect(() => {
+        const newImageUrls = [];
+        if (image) {
+            for (const i of image) {
+                newImageUrls.push(URL.createObjectURL(i));
+            }
+            setPreview(newImageUrls);
+        }
+    }, [image]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Image mutator</h1>
+            </header>
+            <section>
+                <>{
+                    preview && <img src={preview} alt='preview img' />
+                }</>
+                <input type="file" onChange={img}></input>
+            </section>
+        </div>
+    );
 }
 
 export default App;
